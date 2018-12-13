0cf7f0443363839372dbd3f578be5b40f5f53212
David McClure <davidwilliammcclure@gmail.com>
2018-12-12 21:15:44 -0500

    clf_token_texts on parsed HL docs.

--- news_vec/headlines.py
+++ news_vec/headlines.py
@@ -37,14 +37,14 @@ def standardize_hyphens(text):
     return text
 
 
-def scrub_twitter_chars(text):
+def drop_twitter_chars(text):
     return re.sub('[@#]', '', text)
 
 
 STANDARDIZERS = (
     standardize_quotes,
     standardize_hyphens,
-    scrub_twitter_chars,
+    drop_twitter_chars,
 )
 
 def standardize_text(text):
@@ -65,26 +65,33 @@ def is_break_token(token):
     return has_break_char or is_alpha_break_token
 
 
-def clean_clf_text(token):
+def text_clf(token):
     """Drop everything but letters, numbers, and currency ($.,)
     """
     text = re.sub('[^a-z0-9\$\.,]', '', token.text.lower())
 
-    # Drop periods, unless in number.
+    # Drop periods and commas, unless inside of a number.
     if not re.match('[0-9\.,]', text):
         text = re.sub('[\.,]', '', text)
 
     return text
 
 
+def is_clf_token(token):
+    return len(token._.text_clf)
+
+
 def longest_unbroken_span(doc):
     """Find the longest span of tokens without a break token.
     """
+    # Left boundary, break indexes, right boundary.
     break_idxs = [-1, *[t.i for t in doc if t._.is_break_token], len(doc)]
 
+    # Get longest span.
     pairs = sorted(pairwise(break_idxs), key=lambda p: p[1]-p[0])
     i1, i2 = pairs[-1]
 
+    # Slice tokens inside of (but not including) the breaks.
     return doc[i1+1:i2]
 
 
@@ -92,16 +99,23 @@ def clf_tokens(doc):
     """Get filtered tokens for classifier.
     """
     return [
-        t._.text_clf for t in doc._.longest_unbroken_span
-        if len(t._.text_clf)
+        t for t in doc._.longest_unbroken_span
+        if t._.is_clf_token
     ]
 
 
+def clf_token_texts(doc):
+    return [t._.text_clf for t in doc._.clf_tokens]
+
+
+
 Token.set_extension('is_break_token', getter=is_break_token)
-Token.set_extension('text_clf', getter=clean_clf_text)
+Token.set_extension('text_clf', getter=text_clf)
+Token.set_extension('is_clf_token', getter=is_clf_token)
 
 Doc.set_extension('longest_unbroken_span', getter=longest_unbroken_span)
 Doc.set_extension('clf_tokens', getter=clf_tokens)
+Doc.set_extension('clf_token_texts', getter=clf_token_texts)
 
 nlp = spacy.load('en', disable=['ner', 'parser', 'tagger'])
 
--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ from setuptools import setup, find_packages
 setup(
     name='news_vec',
     packages=find_packages(),
+    version='0.1.0',
 )
