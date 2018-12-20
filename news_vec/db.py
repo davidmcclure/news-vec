5f6dc4364d61f8802ba544d06113af2afc8718b7
David McClure <davidwilliammcclure@gmail.com>
2018-12-19 20:27:33 -0500

    Plot article vol trends.

--- news_vec/db.py
+++ news_vec/db.py
@@ -71,6 +71,21 @@ class BaseModel:
 
         session.commit()
 
+    def columns(self):
+        """Get a list of column names.
+
+        Returns: list
+        """
+        return [c.name for c in self.__table__.columns]
+
+    def __iter__(self):
+        """Generate column / value tuples.
+
+        Yields: (key, val)
+        """
+        for key in self.columns():
+            yield (key, getattr(self, key))
+
 
 BaseModel = declarative_base(cls=BaseModel)
 BaseModel.query = session.query_property()
