3d734791060d38e3e44da1497d0489876745683f
David McClure <davidwilliammcclure@gmail.com>
2018-12-23 17:33:29 -0500

    --es_wait

--- bin/train_all_vs_all.py
+++ bin/train_all_vs_all.py
@@ -37,15 +37,16 @@ def freeze_dataset(link_root, headline_root, out_path, skim):
 @click.argument('ds_path', type=click.Path())
 @click.option('--line_enc', type=str, default='lstm')
 @click.option('--eval_every', type=int, default=100000)
+@click.option('--es_wait', type=int, default=5)
 @click.option('--pred_root', type=click.Path())
-def train(ds_path, line_enc, eval_every, pred_root):
+def train(ds_path, line_enc, eval_every, es_wait, pred_root):
     """Train all-vs-all.
     """
     dataset = HeadlineDataset.load(ds_path)
 
     model = Classifier.from_dataset(dataset, line_enc=line_enc)
 
-    trainer = Trainer(model, dataset, eval_every=eval_every)
+    trainer = Trainer(model, dataset, eval_every=eval_every, es_wait=es_wait)
     trainer.train()
 
     preds = trainer.eval_test()
